a5674f245f65003030d5fc05669e211fa98428e8
SuperJappie08 <36795178+SuperJappie08@users.noreply.github.com>
2023-11-15 23:24:57 +0100

    Add test for the Python examples
    This commits add test to prevent regressions in the test.

---
+++ robot-description-builder-py/tests/test_tutorial_examples.py
@@ -0,0 +1,67 @@
+import importlib
+import subprocess
+import sys
+from pathlib import Path
+
+import pytest
+
+# Import the examples as modules relative to this module
+EXAMPLES_PATH = Path(__file__).parents[1].joinpath("examples")
+EXECUTE_PATH = Path(__file__).parents[2]
+
+if EXAMPLES_PATH not in sys.path:
+    sys.path.insert(0, str(EXAMPLES_PATH.absolute()))
+
+urdf_tutorials = {
+    tutorial: importlib.import_module(tutorial)
+    for tutorial in sorted(
+        tutorial.name[:-3]
+        for tutorial in EXAMPLES_PATH.absolute().iterdir()
+        if tutorial.is_file() and tutorial.name[-3:] == ".py"
+    )
+}
+
+
+# Test tutorial 5-7
+@pytest.mark.parametrize("name", sorted(urdf_tutorials.keys())[:-1])
+def test_tutorials(capsys, name):
+    result = urdf_tutorials[name]
+    result.main()
+    cap_py = capsys.readouterr()
+
+    out_rs = subprocess.run(
+        [
+            "cargo",
+            "run",
+            "-q",
+            "--example",
+            name,
+        ],
+        cwd=EXECUTE_PATH,
+        check=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL,
+    ).stdout.decode()
+
+    assert cap_py.out == out_rs
+
+
+def test_tutorial_08():
+    name = "urdf-tutorial-08-macroed"
+    out_py = subprocess.run(
+        [sys.executable, EXAMPLES_PATH.joinpath(name + ".py"), "--width", "0.5"],
+        cwd=EXAMPLES_PATH,
+        check=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL,
+    ).stdout.decode()
+
+    out_rs = subprocess.run(
+        ["cargo", "run", "-q", "--example", name, "--", "0.5"],
+        cwd=EXECUTE_PATH,
+        check=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL,
+    ).stdout.decode()
+
+    assert out_py == out_rs
